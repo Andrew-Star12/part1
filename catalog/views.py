e840eca10b7de98d5cd2bc80bd3135dce5c9d640
Andrew Star <starovoytov.andryusha00@mail.ru>
2024-11-21 12:45:00 +0700

    Библиотекарь

--- catalog/views.py
+++ catalog/views.py
@@ -4,6 +4,18 @@ from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import datetime
+from .forms import RenewBookForm
+from django.shortcuts import render
+from .models import BookInstance
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
 
 def index(request):
     """
@@ -18,6 +30,7 @@ def index(request):
 
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
+    is_librarian = request.user.groups.filter(name='Librarians').exists()
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
@@ -25,7 +38,7 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,
                  'num_authors':num_authors,
-                 'num_visits':num_visits,}
+                 'num_visits':num_visits, 'is_librarian': is_librarian,}
     )
 
 class BookListView(generic.ListView):
@@ -73,3 +86,47 @@ class AllLoanedBooksListView(LoginRequiredMixin, UserPassesTestMixin, generic.Li
     def test_func(self):
         """Проверяем, является ли пользователь библиотекарем."""
         return self.request.user.groups.filter(name='Librarians').exists()
+
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+    """
+    View function for renewing a specific BookInstance by librarian
+    """
+    book_inst = get_object_or_404(BookInstance, pk=pk)
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+        # Create a form instance and populate it with data from the request (binding):
+        form = RenewBookForm(request.POST)
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('all-loaned-books') )
+    # If this is a GET (or any other method) create the default form.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+    return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
+@login_required
+def all_loaned_books(request):
+    # Получаем все книги на прокате
+    bookinstances = BookInstance.objects.filter(status='o').order_by('due_back')
+    # Проверяем, является ли пользователь библиотекарем
+    is_librarian = request.user.groups.filter(name='Librarians').exists()
+    # Передаем информацию в контекст шаблона
+    return render(request, 'catalog/all_loaned_books.html', {
+        'bookinstances': bookinstances,
+        'is_librarian': is_librarian
+    })
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial={'date_of_death':'12/10/2016',}
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name','last_name','date_of_birth','date_of_death']
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
